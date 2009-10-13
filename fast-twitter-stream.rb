e0721558203ec14140366820f153dd3c63922c98
youpy <youpy@buycheapviagraonlinenow.com>
2009-10-13 09:36:04 +0900

    use multipart/x-mixed-replace

--- fast-twitter-stream.rb
+++ fast-twitter-stream.rb
@@ -6,30 +6,13 @@ require 'pit'
 
 # doesn't work on thin and webrick
 set :server, 'mongrel'
-set :public, File.dirname(__FILE__) + '/static'
-
-config = Pit.get("twitter.com", :require => {
-	"username" => "your username in twitter",
-	"password" => "your password in twitter"
-})
 
 get '/' do
   <<HTML
   <html> <head>
     <title>Server Push</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js"></script>
-    <script type="text/javascript" src="/js/DUI.js"></script>
-    <script type="text/javascript" src="/js/Stream.js"></script>
-    <script type="text/javascript">
-    $(function() {
-      var s = new DUI.Stream();
-      s.listen('text/javascript', function(payload) {
-        var status = eval(payload);
-        $('#content').append('<p>' + status.text + '</p>');
-      });
-      s.load('/push');
-	});
-	</script>
+    <script src="/js/server_push.js"></script>
   </head>
   <body>
     <h1>Server Push</h1>
@@ -39,22 +22,46 @@ get '/' do
 HTML
 end
 
+get '/js/server_push.js' do
+  <<JS
+$(function() {
+  var xhr = new XMLHttpRequest();
+  xhr.multipart = true;
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState == 4) {
+      var status = eval(xhr.responseText);
+      $('#content').append('<p>' + status.text + '</p>');
+    }
+  };
+  xhr.open("GET", "/push");
+  xhr.send(null);
+});
+JS
+end
+
 get '/push' do
   boundary = '|||'
-  response['Content-Type'] = 'multipart/mixed; boundary="' + boundary + '"'
+  response['Content-Type'] = 'multipart/x-mixed-replace; boundary="' + boundary + '"'
 
   MultipartResponse.new(boundary, 'text/javascript')
 end
 
 class MultipartResponse
   def initialize(boundary, content_type)
+    @config = Pit.get("twitter.com", :require => {
+        "username" => "your username in twitter",
+        "password" => "your password in twitter"
+      })
+
     @boundary = boundary
     @content_type = content_type
   end
 
   def each
-    TweetStream::Client.new(config[:username], config[:password]).sample do |status|  
-      yield "--#{@boundary}\nContent-Type: #{@content_type}\n(#{status.to_json})"
+    yield "--#{@boundary}\n"
+
+    TweetStream::Client.new(@config['username'], @config['password']).sample do |status|  
+      yield "Content-Type: #{@content_type}\n\n(#{status.to_json})\n--#{@boundary}\n"
     end  
   end
 end
